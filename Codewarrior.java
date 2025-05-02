1709889b23b577137ddca627c5c64ccf5d1784cd
Ntokozo <ntokntshangase@gmail.com>
2025-05-02 21:56:37 +0200

    Create Codewarrior.java

---
+++ Codewarrior.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Codewarrior 
+{
+    @SuppressWarnings("unchecked")
+    public static List<Integer>[] arrayOfLists = new List[] 
+    {
+        Arrays.asList(1, 2, 3),
+        Arrays.asList(4, 5, 6),
+        Arrays.asList(7, 8, 9)
+    };
+}
